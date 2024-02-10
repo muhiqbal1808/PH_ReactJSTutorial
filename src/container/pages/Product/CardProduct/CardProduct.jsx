414a03bc047deeaf0bd590316f808113355bde77
Muh Iqbal <muhammadiqbal18081996@gmail.com>
2024-02-10 20:48:36 +0800

    #14

--- src/container/pages/Product/CardProduct/CardProduct.jsx
+++ src/container/pages/Product/CardProduct/CardProduct.jsx
@@ -1,37 +1,7 @@
 import React, { Component } from "react";
+import Counter from "./Counter";
 
 class CardProduct extends Component {
-  state = {
-    order: 4,
-  };
-
-  handleCounterChange = (newValue) => {
-    this.props.onCounterChange(newValue);
-  };
-
-  handlePlus = () => {
-    this.setState(
-      {
-        order: this.state.order + 1,
-      },
-      () => {
-        this.handleCounterChange(this.state.order);
-      }
-    );
-  };
-
-  handleMinus = () => {
-    if (this.state.order > 0) {
-      this.setState(
-        {
-          order: this.state.order - 1,
-        },
-        () => {
-          this.handleCounterChange(this.state.order);
-        }
-      );
-    }
-  };
   render() {
     return (
       <div className="card">
@@ -45,15 +15,9 @@ class CardProduct extends Component {
           PINZY Headset Bluetooth Sport Magnetic design Original Pz03
         </p>
         <p className="product-price">Rp. 13.500</p>
-        <div className="counter">
-          <button className="minus" onClick={this.handleMinus}>
-            -
-          </button>
-          <input type="text" value={this.state.order} />
-          <button className="plus" onClick={this.handlePlus}>
-            +
-          </button>
-        </div>
+        <Counter
+          onCounterChange={(value) => this.props.onCounterChange(value)}
+        />
       </div>
     );
   }
