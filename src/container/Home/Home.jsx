69d25648f6c54580c18a1040b572b2b4febd9922
Muh Iqbal <muhammadiqbal18081996@gmail.com>
2024-02-11 08:28:54 +0800

    #23

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component, createContext } from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 import "./Home.css";
@@ -9,53 +9,28 @@ import Product from "../pages/Product/Product";
 import YoutubeComponentPage from "../pages/YoutubeComponentPage/YoutubeComponentPage";
 import DetailPost from "../pages/BlogPost/DetailPost/DetailPost";
 
-export const RootContext = createContext();
-const Provider = RootContext.Provider;
+import GlobalProvider from "../../context/context";
 
 class Home extends Component {
-  state = {
-    totalOrder: 5,
-  };
-
-  dispatch = (action) => {
-    if (action.type === "PLUS_ORDER") {
-      return this.setState({
-        totalOrder: this.state.totalOrder + 1,
-      });
-    }
-    if (action.type === "MINUS_ORDER") {
-      return this.setState({
-        totalOrder: this.state.totalOrder - 1,
-      });
-    }
-  };
-
   render() {
     return (
       <BrowserRouter>
-        <Provider
-          value={{
-            state: this.state,
-            dispatch: this.dispatch,
-          }}
-        >
-          <div className="navigation">
-            <Link to="/">Blog Post</Link>
-            <Link to="/product">Product</Link>
-            <Link to="/lifecycle">Lifecycle</Link>
-            <Link to="/youtube">Youtube</Link>
-          </div>
-          <Routes>
-            <Route path="/" Component={BlogPost} />
-            <Route path="/detail-post/:id" Component={DetailPost} />
-            <Route path="/product" Component={Product} />
-            <Route path="/lifecycle" Component={LifeCycleComponent} />
-            <Route path="/youtube" Component={YoutubeComponentPage} />
-          </Routes>
-        </Provider>
+        <div className="navigation">
+          <Link to="/">Blog Post</Link>
+          <Link to="/product">Product</Link>
+          <Link to="/lifecycle">Lifecycle</Link>
+          <Link to="/youtube">Youtube</Link>
+        </div>
+        <Routes>
+          <Route path="/" Component={BlogPost} />
+          <Route path="/detail-post/:id" Component={DetailPost} />
+          <Route path="/product" Component={Product} />
+          <Route path="/lifecycle" Component={LifeCycleComponent} />
+          <Route path="/youtube" Component={YoutubeComponentPage} />
+        </Routes>
       </BrowserRouter>
     );
   }
 }
 
-export default Home;
+export default GlobalProvider(Home);
---
+++ src/context/context.jsx
@@ -0,0 +1,43 @@
+import React, { Component, createContext } from "react";
+
+const RootContext = createContext();
+
+// Provider
+const Provider = RootContext.Provider;
+
+const GlobalProvider = (Children) => {
+  return class ParentComponent extends Component {
+    state = {
+      totalOrder: 5,
+    };
+
+    dispatch = (action) => {
+      if (action.type === "PLUS_ORDER") {
+        return this.setState({
+          totalOrder: this.state.totalOrder + 1,
+        });
+      }
+      if (action.type === "MINUS_ORDER") {
+        return this.setState({
+          totalOrder: this.state.totalOrder - 1,
+        });
+      }
+    };
+    render() {
+      return (
+        <Provider
+          value={{
+            state: this.state,
+            dispatch: this.dispatch,
+          }}
+        >
+          <Children {...this.props} />
+        </Provider>
+      );
+    }
+  };
+};
+
+export default GlobalProvider;
+
+// Consumer
